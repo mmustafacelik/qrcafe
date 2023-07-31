a492ce841fdff2517ab3a7ceb0819c79c5b2b22b
Mustafa Çelik <mustik1518@gmail.com>
2023-07-31 23:54:57 +0300

    added packages and some main issue

---
+++ lib/navigate.dart
@@ -0,0 +1,18 @@
+import 'package:go_router/go_router.dart';
+import 'package:qrcafe/main.dart';
+
+enum AppRoute {
+  home,
+}
+
+final goRouter = GoRouter(
+  initialLocation: '/home_page',
+  debugLogDiagnostics: true,
+  routes: [
+    GoRoute(
+      path: '/home_page',
+      name: AppRoute.home.name,
+      builder: (context, state) => const MyHomePage(),
+    ),
+  ],
+);
